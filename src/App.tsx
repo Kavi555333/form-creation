b3cb60405a45da1a1ac0bf37b866f5c5c10d8883
Arunkavi <arunkavi@fipsar.com>
2026-03-12 06:51:48 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import  { useState } from 'react';
+//import  { useState } from 'react';
 import './App.css'
-import EnrollmentPage from './components/EnrollmentPage';
-import FeedbackModal from './components/FeedbackModal'
+// import EnrollmentPage from './components/EnrollmentPage';
+// import FeedbackModal from './components/FeedbackModal'
 import Header from './components/Header'
-import SupportSection from './components/SupportSection'
+// import SupportSection from './components/SupportSection'
+import SignUpForm from './components/SignUpForm';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<'home' | 'enrollment'>('home');
+  // const [currentPage, setCurrentPage] = useState<'home' | 'enrollment'>('home');
 
   // if (currentPage === 'enrollment') {
   //   return <EnrollmentPage onBack={() => setCurrentPage('home')} />;
   // }
   return (
     <>
-      <FeedbackModal/>
+      {/* <FeedbackModal/>
 
  {currentPage === 'enrollment' ? (
         <EnrollmentPage onBack={() => setCurrentPage('home')} />
       ) :(
       <>
       <Header/>
+      <SignUpForm/>
       <main>
       <SupportSection onNavigateToEnrollment={() => setCurrentPage('enrollment')}/>
         </main>
       
       </>
       
-      )}
+      )} */}
 
-
+<Header/>
+      <SignUpForm/>
       
     </>
   )
